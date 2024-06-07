6e65ca42272f4d50f97b0d2d9d471a21960649f2
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-06-07 20:07:07 +1200

    fix: update elastic machine setup (#6)

--- telemetry/main.go
+++ telemetry/main.go
@@ -6,7 +6,6 @@ import (
 	"github.com/dogmatiq/ferrite"
 	"github.com/pulumi/pulumi-cloudflare/sdk/v5/go/cloudflare"
 	"github.com/pulumi/pulumi-gcp/sdk/v7/go/gcp/compute"
-	"github.com/pulumi/pulumi-gcp/sdk/v7/go/gcp/storage"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
@@ -17,17 +16,12 @@ var (
 	GOOGLE_PROJECT = ferrite.
 			String("GOOGLE_PROJECT", "GCP Project").
 			Required()
+	GOOGLE_SERVICE_ACCOUNT = ferrite.
+				String("GOOGLE_SERVICE_ACCOUNT", "Service account linked to vm").
+				Required()
 	CLOUDFLARE_ZONE_ID = ferrite.
 				String("CLOUDFLARE_ZONE_ID", "Cloudflare zone id").
 				Required()
-	SIGNOZ_PATCH = ferrite.
-			String("SIGNOZ_PATCH", "Signoz config patch").
-			Required()
-	GOOGLE_SERVICE_ACCOUNT = ferrite.
-		// See: https://www.pulumi.com/registry/packages/gcp/api-docs/storage/getobjectsignedurl/#credentials_go
-		// `export GOOGLE_SERVICE_ACCOUNT=$(cat credentials.json)`
-		String("GOOGLE_SERVICE_ACCOUNT", "Google service account credentials to obtain a signed url").
-		Required()
 	CLOUDFLARE_API_TOKEN = ferrite.
 				String("CLOUDFLARE_API_TOKEN", "Cloudflare API token").
 				Required()
@@ -37,43 +31,6 @@ func main() {
 	ferrite.Init()
 
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		configPatchUrl, err := storage.GetObjectSignedUrl(ctx, &storage.GetObjectSignedUrlArgs{
-			Bucket:      "skulpture-shared-resources",
-			Path:        fmt.Sprintf("telemetry/patch/%s", SIGNOZ_PATCH.Value()),
-			Credentials: pulumi.StringRef(GOOGLE_SERVICE_ACCOUNT.Value()),
-		})
-		if err != nil {
-			return err
-		}
-
-		_, err = compute.NewFirewall(ctx, "allow-collector", &compute.FirewallArgs{
-			Name:        pulumi.String("allow-collector"),
-			Network:     pulumi.String("shared-resources-network"),
-			Description: pulumi.StringPtr("Allow access to OpenTelemetry collector"),
-			Allows: compute.FirewallAllowArray{
-				&compute.FirewallAllowArgs{
-					Protocol: pulumi.String("tcp"),
-					Ports: pulumi.ToStringArray([]string{
-						"4317",
-						"4318",
-						"8880",
-						"13133",
-					},
-					),
-				},
-			},
-			SourceRanges: pulumi.ToStringArray([]string{
-				"0.0.0.0/0",
-			},
-			),
-			TargetTags: pulumi.StringArray{
-				pulumi.String("allow-collector"),
-			},
-		})
-		if err != nil {
-			return err
-		}
-
 		static, err := compute.NewAddress(ctx, COMPUTE_INSTANCE_NAME.Value(), &compute.AddressArgs{
 			Name:   pulumi.String(COMPUTE_INSTANCE_NAME.Value()),
 			Region: pulumi.String("australia-southeast1"),
@@ -84,12 +41,11 @@ func main() {
 
 		instance, err := compute.NewInstance(ctx, COMPUTE_INSTANCE_NAME.Value(), &compute.InstanceArgs{
 			Name:        pulumi.String(COMPUTE_INSTANCE_NAME.Value()),
-			MachineType: pulumi.String("e2-small"),
+			MachineType: pulumi.String("e2-standard-2"),
 			Zone:        pulumi.String("australia-southeast1-a"),
 			Tags: pulumi.ToStringArray([]string{
 				"allow-cloudflare",
 				"allow-ssh",
-				"allow-collector",
 			}),
 			NetworkInterfaces: compute.InstanceNetworkInterfaceArray{
 				&compute.InstanceNetworkInterfaceArgs{
@@ -102,24 +58,27 @@ func main() {
 				},
 			},
 			// Docker setup on Debian 12: https://www.thomas-krenn.com/en/wiki/Docker_installation_on_Debian_12
-			// Signoz setup: https://signoz.io/docs/install/docker-swarm/
-			// - https://signoz.io/docs/monitor-http-endpoints/
-			// - https://signoz.io/docs/userguide/otlp-http-enable-cors/
-			// - https://signoz.io/docs/operate/
+			// Permanently increase vm.max_map_count value: https://thetechdarts.com/how-to-change-default-vm-max_map_count-on-linux/
 			MetadataStartupScript: pulumi.String(fmt.Sprintf(`#! /bin/bash 
 				sudo apt update &&
-				sudo apt install git ca-certificates curl gnupg apt-transport-https gpg -y &&
+				sudo apt install certbot python3-certbot-dns-cloudflare make git ca-certificates curl gnupg apt-transport-https gpg -y &&
 				curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker.gpg &&
 				echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/debian bookworm stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null &&
 				sudo apt update &&
 				sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose -y &&
-				git clone -b v0.46.0-389f22cf6 https://github.com/SigNoz/signoz.git && cd signoz/deploy/ &&
-				curl '%s' > otel-collector-config.patch &&
-				git apply otel-collector-config.patch &&
-				sudo docker swarm init &&
-				sudo docker stack deploy -c docker-swarm/clickhouse-setup/docker-compose.yaml signoz &&
-				sudo docker stack services signoz
-				EOF`, configPatchUrl.SignedUrl)),
+				sudo grep -qxF 'vm.max_map_count=262144' /etc/sysctl.conf || echo vm.max_map_count=262144 | sudo tee -a /etc/sysctl.conf &&
+				sudo sysctl -p &&
+				sudo mkdir -p /etc/letsencrypt/renewal-hooks/deploy && 
+				echo "dns_cloudflare_api_token = %s" | sudo tee /etc/letsencrypt/dnscloudflare.ini &&
+				echo "#! /bin/bash sudo docker service ls -q | xargs -n1 sudo docker service update --force" | sudo tee /etc/letsencrypt/renewal-hooks/deploy/reload-services.sh &&
+				sudo chmod 0600 /etc/letsencrypt/dnscloudflare.ini &&
+				sudo chmod +x /etc/letsencrypt/renewal-hooks/deploy/reload-services.sh &&
+				sudo chmod 0600 /etc/letsencrypt/renewal-hooks/deploy/reload-services.sh &&
+				sudo certbot certonly -d telemetry.skulpture.xyz \
+					--dns-cloudflare --dns-cloudflare-credentials /etc/letsencrypt/dnscloudflare.ini \
+					--non-interactive --agree-tos \
+					--register-unsafely-without-email \
+					--dns-cloudflare-propagation-seconds 60`, CLOUDFLARE_API_TOKEN.Value())),
 			Scheduling: compute.InstanceSchedulingArgs{
 				AutomaticRestart:  pulumi.Bool(true),
 				OnHostMaintenance: pulumi.String("MIGRATE"),
@@ -133,6 +92,12 @@ func main() {
 					Size:  pulumi.Int(20),
 				},
 				AutoDelete: pulumi.Bool(false),
+			},
+			ServiceAccount: compute.InstanceServiceAccountArgs{
+				Email: pulumi.StringPtr(GOOGLE_SERVICE_ACCOUNT.Value()),
+				Scopes: pulumi.ToStringArray([]string{
+					"cloud-platform",
+				}),
 			},
 		})
 		if err != nil {
