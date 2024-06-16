fedabf5efb09767757701d24ec0c497ea058c81b
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-06-16 20:54:53 +1200

    chore: increase disk size

--- rollout/main.go
+++ rollout/main.go
@@ -93,7 +93,7 @@ func main() {
 				InitializeParams: &compute.InstanceBootDiskInitializeParamsArgs{
 					Image: pulumi.String("debian-12-bookworm-v20240515"),
 					Type:  pulumi.String("pd-standard"),
-					Size:  pulumi.Int(10),
+					Size:  pulumi.Int(30),
 				},
 				AutoDelete: pulumi.Bool(false),
 			},
