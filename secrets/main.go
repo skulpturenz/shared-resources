cdd6c739c67f82228adc154721cc10c66a60d8f7
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-06-19 11:46:07 +0000

    fix: project scope

--- secrets/main.go
+++ secrets/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log/slog"
 	"os"
 
@@ -10,6 +11,10 @@ import (
 )
 
 var (
+	PROJECT = ferrite.
+		String("Project", "Project to load environment for").
+		WithDefault("").
+		Required()
 	DSN = ferrite.
 		String("DSN", "SQLite connection string").
 		Required()
@@ -22,7 +27,7 @@ func init() {
 	}
 	defer db.Close()
 
-	rows, err := db.Query("SELECT key, value FROM secrets WHERE deprecated = 0;")
+	rows, err := db.Query(fmt.Sprintf("SELECT key, value FROM secrets WHERE deprecated = 0 AND (project = %s OR project = '*');", PROJECT.Value()))
 	if err != nil {
 		panic(err)
 	}
