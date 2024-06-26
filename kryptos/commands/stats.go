7b2a31d3c23befe321f0a30246fc6cf528e22dc0
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-06-26 10:58:04 +0000

    feat: krypots stats

---
+++ kryptos/commands/stats.go
@@ -0,0 +1,32 @@
+package commands
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"io"
+	"skulpture/kryptos/kryptos"
+	"text/tabwriter"
+)
+
+type Stat struct {
+	Db   *sql.DB
+	View io.Writer
+}
+
+func (command *Stat) Execute(ctx context.Context) error {
+	w := tabwriter.NewWriter(command.View, 1, 4, 1, ' ', 0)
+
+	fmt.Fprintln(w, "Key\tProject\tVersions")
+
+	envStats, err := kryptos.Stats(ctx, command.Db)
+	if err != nil {
+		return err
+	}
+
+	for _, stat := range envStats {
+		fmt.Fprintf(w, "%s\t%s\t%d", stat.Key, stat.Project, stat.Count)
+	}
+
+	return nil
+}
