d10ee5c12145b2a2e70c5ba6cffac17c2ce94664
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-06-27 11:37:01 +0000

    refactor: print to View with fprint

--- kryptos/commands/info.go
+++ kryptos/commands/info.go
@@ -13,15 +13,15 @@ type Info struct {
 
 func (command *Info) Execute(ctx context.Context) error {
 	info := []string{
-		fmt.Sprintf("Project: %s\n", kryptos.PROJECT.Value()),
-		fmt.Sprintf("Database driver: %s\n", kryptos.DB_DRIVER.Value()),
-		fmt.Sprintf("Database connection string: %s\n", kryptos.DB_CONNECTION_STRING.Value()),
-		fmt.Sprintf("Encryption key: %s\n", kryptos.ENCRYPTION_KEY.Value()),
-		fmt.Sprintf("Version: v%s\n", kryptos.VERSION),
+		fmt.Sprintf("Project: %s", kryptos.PROJECT.Value()),
+		fmt.Sprintf("Database driver: %s", kryptos.DB_DRIVER.Value()),
+		fmt.Sprintf("Database connection string: %s", kryptos.DB_CONNECTION_STRING.Value()),
+		fmt.Sprintf("Encryption key: %s", kryptos.ENCRYPTION_KEY.Value()),
+		fmt.Sprintf("Version: v%s", kryptos.VERSION),
 	}
 
 	for _, info := range info {
-		_, err := command.View.Write([]byte(info))
+		_, err := fmt.Fprintln(command.View, info)
 		if err != nil {
 			return err
 		}
