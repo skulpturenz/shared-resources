2586f02c0660e26439afd9ef57e128c132be3722
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-07-16 08:02:50 +0000

    chore: add Plus Jakarta Sans

--- authnz-theme/src/main.tsx
+++ authnz-theme/src/main.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react-refresh/only-export-components */
 import { createRoot } from "react-dom/client";
 import { StrictMode, lazy, Suspense } from "react";
+import "./index.css";
 
 // The following block can be uncommented to test a specific page with `yarn dev`
 // Don't forget to comment back or your bundle size will increase
--- authnz-theme/tailwind.config.js
+++ authnz-theme/tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	darkMode: ["class"],
@@ -17,6 +19,9 @@ module.exports = {
 			},
 		},
 		extend: {
+			fontFamily: {
+				sans: ["Plus Jakarta Sans", ...defaultTheme.fontFamily.sans],
+			},
 			colors: {
 				border: "hsl(var(--border))",
 				input: "hsl(var(--input))",
