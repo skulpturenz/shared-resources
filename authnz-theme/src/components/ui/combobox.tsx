b2c58c63e6867b097ae2e8731151e2472d5c7580
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-07-17 08:36:14 +0000

    style: remove unused use client directive and import formatting

--- authnz-theme/src/components/ui/combobox.tsx
+++ authnz-theme/src/components/ui/combobox.tsx
@@ -1,8 +1,5 @@
-"use client";
-
 import * as React from "react";
 import { Check, ChevronsUpDown } from "lucide-react";
-
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import {
