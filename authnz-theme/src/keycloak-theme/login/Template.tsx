9f9937565afc892c9727044bca6a425ba825ab72
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-07-18 11:57:51 +0000

    feat: update dark mode icon

--- authnz-theme/src/keycloak-theme/login/Template.tsx
+++ authnz-theme/src/keycloak-theme/login/Template.tsx
@@ -27,12 +27,12 @@ import {
 } from "@/components/ui/dropdown-menu";
 import {
 	Sun,
-	Moon,
 	CircleAlert,
 	CircleX,
 	Info,
 	CircleCheck,
 	RotateCcw,
+	MoonStar,
 } from "lucide-react";
 import {
 	LogoLight,
@@ -331,7 +331,7 @@ const TemplateWithoutTheme = (props: TemplateProps<KcContext, I18n>) => {
 									<DropdownMenuTrigger asChild>
 										<Button variant="outline" size="icon">
 											<Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-											<Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+											<MoonStar className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 text-primary" />
 											<span className="sr-only">
 												Toggle theme
 											</span>
