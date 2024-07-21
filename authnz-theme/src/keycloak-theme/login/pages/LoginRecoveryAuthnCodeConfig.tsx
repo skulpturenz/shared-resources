a19a6b3eb60dfdbd9343e9cad6c4baf402ec200a
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-07-21 06:16:31 +0000

    feat: add sonner and toast, handlers to print, download and copy recovery codes

--- authnz-theme/src/keycloak-theme/login/pages/LoginRecoveryAuthnCodeConfig.tsx
+++ authnz-theme/src/keycloak-theme/login/pages/LoginRecoveryAuthnCodeConfig.tsx
@@ -1,17 +1,19 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { getKcClsx, type KcClsx } from "keycloakify/login/lib/kcClsx";
-import { useInsertScriptTags } from "keycloakify/tools/useInsertScriptTags";
 import type { PageProps } from "keycloakify/login/pages/PageProps";
 import type { KcContext } from "../KcContext";
 import type { I18n } from "../i18n";
 import { CircleAlert, Copy, Download, Printer } from "lucide-react";
-import { Ol } from "@/components/typography";
+import { Ol, P } from "@/components/typography";
 import { Form, FormGroup } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import type { CheckedState } from "@radix-ui/react-checkbox";
+import { Toaster } from "@/components/ui/sonner";
+import { toast } from "sonner";
+import { Portal } from "@/components/ui/portal";
 
 export const LoginRecoveryAuthnCodeConfig = (
 	props: PageProps<
@@ -30,135 +32,10 @@ export const LoginRecoveryAuthnCodeConfig = (
 
 	const { msg, msgStr } = i18n;
 
-	const { insertScriptTags } = useInsertScriptTags({
-		componentOrHookName: "LoginRecoveryAuthnCodeConfig",
-		scriptTags: [
-			{
-				type: "text/javascript",
-				textContent: `
-
-                    /* copy recovery codes  */
-                    function copyRecoveryCodes() {
-                        var tmpTextarea = document.createElement("textarea");
-                        var codes = document.getElementById("kc-recovery-codes-list").getElementsByTagName("li");
-                        for (i = 0; i < codes.length; i++) {
-                            tmpTextarea.value = tmpTextarea.value + codes[i].innerText + "\\n";
-                        }
-                        document.body.appendChild(tmpTextarea);
-                        tmpTextarea.select();
-                        document.execCommand("copy");
-                        document.body.removeChild(tmpTextarea);
-                    }
-
-                    var copyButton = document.getElementById("copyRecoveryCodes");
-                    copyButton && copyButton.addEventListener("click", function () {
-                        copyRecoveryCodes();
-                    });
-
-                    /* download recovery codes  */
-                    function formatCurrentDateTime() {
-                        var dt = new Date();
-                        var options = {
-                            month: 'long',
-                            day: 'numeric',
-                            year: 'numeric',
-                            hour: 'numeric',
-                            minute: 'numeric',
-                            timeZoneName: 'short'
-                        };
-
-                        return dt.toLocaleString('en-US', options);
-                    }
-
-                    function parseRecoveryCodeList() {
-                        var recoveryCodes = document.querySelectorAll(".kc-recovery-codes-list li");
-                        var recoveryCodeList = "";
-
-                        for (var i = 0; i < recoveryCodes.length; i++) {
-                            var recoveryCodeLiElement = recoveryCodes[i].innerText;
-                            recoveryCodeList += recoveryCodeLiElement + "\\r\\n";
-                        }
-
-                        return recoveryCodeList;
-                    }
-
-                    function buildDownloadContent() {
-                        var recoveryCodeList = parseRecoveryCodeList();
-                        var dt = new Date();
-                        var options = {
-                            month: 'long',
-                            day: 'numeric',
-                            year: 'numeric',
-                            hour: 'numeric',
-                            minute: 'numeric',
-                            timeZoneName: 'short'
-                        };
-
-                        return fileBodyContent =
-                            "${msgStr("recovery-codes-download-file-header")}\\n\\n" +
-                            recoveryCodeList + "\\n" +
-                            "${msgStr("recovery-codes-download-file-description")}\\n\\n" +
-                            "${msgStr("recovery-codes-download-file-date")} " + formatCurrentDateTime();
-                    }
-
-                    function setUpDownloadLinkAndDownload(filename, text) {
-                        var el = document.createElement('a');
-                        el.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-                        el.setAttribute('download', filename);
-                        el.style.display = 'none';
-                        document.body.appendChild(el);
-                        el.click();
-                        document.body.removeChild(el);
-                    }
-
-                    function downloadRecoveryCodes() {
-                        setUpDownloadLinkAndDownload('kc-download-recovery-codes.txt', buildDownloadContent());
-                    }
-
-                    var downloadButton = document.getElementById("downloadRecoveryCodes");
-                    downloadButton && downloadButton.addEventListener("click", downloadRecoveryCodes);
-
-                    /* print recovery codes */
-                    function buildPrintContent() {
-                        var recoveryCodeListHTML = document.getElementById('kc-recovery-codes-list').innerHTML;
-                        var styles =
-                            \`@page { size: auto;  margin-top: 0; }
-                            body { width: 480px; }
-                            div { list-style-type: none; font-family: monospace }
-                            p:first-of-type { margin-top: 48px }\`;
-
-                        return printFileContent =
-                            "<html><style>" + styles + "</style><body>" +
-                            "<title>kc-download-recovery-codes</title>" +
-                            "<p>${msgStr("recovery-codes-download-file-header")}</p>" +
-                            "<div>" + recoveryCodeListHTML + "</div>" +
-                            "<p>${msgStr("recovery-codes-download-file-description")}</p>" +
-                            "<p>${msgStr("recovery-codes-download-file-date")} " + formatCurrentDateTime() + "</p>" +
-                            "</body></html>";
-                    }
-
-                    function printRecoveryCodes() {
-                        var w = window.open();
-                        w.document.write(buildPrintContent());
-                        w.print();
-                        w.close();
-                    }
-
-                    var printButton = document.getElementById("printRecoveryCodes");
-                    printButton && printButton.addEventListener("click", printRecoveryCodes);
-                `,
-			},
-		],
-	});
-
 	const [areRecoveryCodesSaved, setAreRecoveryCodesSaved] =
 		React.useState(false);
 	const onChangeAreRecoveryCodesSaved = (checked: CheckedState) =>
 		setAreRecoveryCodesSaved(checked.valueOf() as boolean);
-
-	useEffect(() => {
-		insertScriptTags();
-	}, []);
 
 	const onConfirmSavedRecoveryCodes = () => {
 		//@ts-expect-error: This is code from the original theme, we trust it.
@@ -166,6 +43,122 @@ export const LoginRecoveryAuthnCodeConfig = (
 			"saveRecoveryAuthnCodesBtn",
 			//@ts-expect-error: This is code from the original theme, we trust it.
 		).disabled = !areRecoveryCodesSaved;
+	};
+
+	const formatCurrentDateTime = () => {
+		const currentDate = new Date();
+		const options: Intl.DateTimeFormatOptions = {
+			month: "long",
+			day: "numeric",
+			year: "numeric",
+			hour: "numeric",
+			minute: "numeric",
+			timeZoneName: "short",
+		};
+
+		return currentDate.toLocaleString("en-US", options);
+	};
+
+	const onCopyRecoveryCodes = () => {
+		const parseRecoveryCodeList = () =>
+			Array.from(
+				document.querySelectorAll(
+					".kc-recovery-codes-list",
+				) as NodeListOf<HTMLElement>,
+			)
+				.map(element => element.innerText)
+				.join("\r\n");
+
+		navigator.clipboard.writeText(parseRecoveryCodeList());
+
+		toast("Recovery codes copied", {
+			description: formatCurrentDateTime(),
+		});
+	};
+
+	const onDownloadRecoveryCodes = () => {
+		const parseRecoveryCodeList = () =>
+			Array.from(
+				document.querySelectorAll(
+					".kc-recovery-codes-list",
+				) as NodeListOf<HTMLElement>,
+			)
+				.map(element => element.innerText)
+				.join("\r\n");
+
+		const buildDownloadContent = () =>
+			`${msgStr("recovery-codes-download-file-header")}\n\n` +
+			parseRecoveryCodeList() +
+			"\n" +
+			`${msgStr("recovery-codes-download-file-description")}\n\n` +
+			`${msgStr("recovery-codes-download-file-date")} ` +
+			formatCurrentDateTime();
+
+		const setUpDownloadLinkAndDownload = (
+			filename: string,
+			text: string,
+		) => {
+			const el = document.createElement("a");
+			el.setAttribute(
+				"href",
+				"data:text/plain;charset=utf-8," + encodeURIComponent(text),
+			);
+			el.setAttribute("download", filename);
+			el.style.display = "none";
+			document.body.appendChild(el);
+			el.click();
+			document.body.removeChild(el);
+		};
+
+		setUpDownloadLinkAndDownload(
+			"kc-download-recovery-codes.txt",
+			buildDownloadContent(),
+		);
+
+		toast("Recovery codes downloaded", {
+			description: formatCurrentDateTime(),
+		});
+	};
+
+	const onPrintRecoveryCodes = () => {
+		function buildPrintContent() {
+			const recoveryCodeListHTML = document.getElementById(
+				"kc-recovery-codes-list",
+			)?.innerHTML;
+			const styles = `@page { size: auto;  margin-top: 0; }
+				body { width: 480px; }
+				div { list-style-type: none; font-family: monospace }
+				p:first-of-type { margin-top: 48px }`;
+
+			return (
+				"<html><style>" +
+				styles +
+				"</style><body>" +
+				"<title>kc-download-recovery-codes</title>" +
+				`<p>${msgStr("recovery-codes-download-file-header")}</p>` +
+				"<div>" +
+				recoveryCodeListHTML +
+				"</div>" +
+				`<p>${msgStr("recovery-codes-download-file-description")}</p>` +
+				`<p>${msgStr("recovery-codes-download-file-date")} ` +
+				formatCurrentDateTime() +
+				"</p>" +
+				"</body></html>"
+			);
+		}
+
+		function printRecoveryCodes() {
+			const w = window.open();
+			w?.document.write(buildPrintContent());
+			w?.print();
+			w?.close();
+		}
+
+		printRecoveryCodes();
+
+		toast("Recovery codes printed", {
+			description: formatCurrentDateTime(),
+		});
 	};
 
 	return (
@@ -191,31 +184,48 @@ export const LoginRecoveryAuthnCodeConfig = (
 				id="kc-recovery-codes-list"
 				className={kcClsx("kcRecoveryCodesList")}>
 				{recoveryAuthnCodesConfigBean.generatedRecoveryAuthnCodesList.map(
-					(code, index) => (
-						<li key={index}>
-							<span>{index + 1}:</span> {code.slice(0, 4)}-
-							{code.slice(4, 8)}-{code.slice(8)}
+					code => (
+						<li className="kc-recovery-codes-list" key={code}>
+							<P>
+								{code.slice(0, 4)}-{code.slice(4, 8)}-
+								{code.slice(8)}
+							</P>
 						</li>
 					),
 				)}
 			</Ol>
 
 			<FormGroup flexDirection="row">
-				<Button variant="outline" className="w-full flex gap-2">
+				<Button
+					id="printRecoveryCodes"
+					variant="outline"
+					className="w-full flex gap-2"
+					onClick={onPrintRecoveryCodes}>
 					<Printer className="h-4 w-4" />
 					{msg("recovery-codes-print")}
 				</Button>
-				<Button variant="outline" className="w-full flex gap-2">
+				<Button
+					id="downloadRecoveryCodes"
+					variant="outline"
+					className="w-full flex gap-2"
+					onClick={onDownloadRecoveryCodes}>
 					<Download className="h-4 w-4" />
 					{msg("recovery-codes-download")}
 				</Button>
-				<Button variant="outline" className="w-full flex gap-2">
+				<Button
+					id="copyRecoveryCodes"
+					variant="outline"
+					className="w-full flex gap-2"
+					onClick={onCopyRecoveryCodes}>
 					<Copy className="h-4 w-4" />
 					{msg("recovery-codes-copy")}
 				</Button>
 			</FormGroup>
 
-			<FormGroup flexDirection="row" className="mt-10 my-4">
+			<FormGroup
+				id="kc-recovery-codes-settings-form"
+				flexDirection="row"
+				className="mt-12 my-6">
 				<Checkbox
 					id="kcRecoveryCodesConfirmationCheck"
 					name="kcRecoveryCodesConfirmationCheck"
@@ -284,6 +294,10 @@ export const LoginRecoveryAuthnCodeConfig = (
 					</Button>
 				)}
 			</Form>
+
+			<Portal>
+				<Toaster />
+			</Portal>
 		</Template>
 	);
 };
