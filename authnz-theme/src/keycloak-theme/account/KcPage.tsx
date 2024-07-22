6a6838c80a248524aaba19504c002da8fe3365ed
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-07-22 10:31:44 +0000

    chore: eject remaining pages

--- authnz-theme/src/keycloak-theme/account/KcPage.tsx
+++ authnz-theme/src/keycloak-theme/account/KcPage.tsx
@@ -3,7 +3,7 @@ import type { ClassKey } from "keycloakify/account";
 import type { KcContext } from "./KcContext";
 import { useI18n } from "./i18n";
 import DefaultPage from "keycloakify/account/DefaultPage";
-import Template from "keycloakify/account/Template";
+import Template from "./Template";
 
 export default function KcPage(props: { kcContext: KcContext }) {
 	const { kcContext } = props;
