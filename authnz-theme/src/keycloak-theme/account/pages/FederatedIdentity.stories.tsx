c539b16d472db70dc6942e7e6307f4466d48afaa
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-07-23 06:44:28 +0000

    chore: add stories

---
+++ authnz-theme/src/keycloak-theme/account/pages/FederatedIdentity.stories.tsx
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { createKcPageStory } from "../KcPageStory";
+
+const { KcPageStory } = createKcPageStory({ pageId: "federatedIdentity.ftl" });
+
+const meta = {
+    title: "account/federatedIdentity.ftl",
+    component: KcPageStory
+} satisfies Meta<typeof KcPageStory>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    render: () => <KcPageStory />
+};
+
+export const NotConnected: Story = {
+    render: () => (
+        <KcPageStory
+            kcContext={{
+                pageId: "federatedIdentity.ftl",
+                federatedIdentity: {
+                    identities: [
+                        {
+                            providerId: "google",
+                            displayName: "keycloak-oidc",
+                            connected: false
+                        }
+                    ],
+                    removeLinkPossible: true
+                }
+            }}
+        />
+    )
+};
