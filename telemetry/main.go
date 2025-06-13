4a7876df238903bf4f45833976a491a631a68bd4
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-06-13 17:55:10 +1200

    chore: scale telemetry vm and services down

--- telemetry/main.go
+++ telemetry/main.go
@@ -44,7 +44,7 @@ func main() {
 
 		instance, err := compute.NewInstance(ctx, COMPUTE_INSTANCE_NAME.Value(), &compute.InstanceArgs{
 			Name:        pulumi.String(COMPUTE_INSTANCE_NAME.Value()),
-			MachineType: pulumi.String("e2-small") // pulumi.String("e2-highmem-2"),
+			MachineType: pulumi.String("e2-medium"),
 			Zone:        pulumi.String("australia-southeast1-a"),
 			Tags: pulumi.ToStringArray([]string{
 				"allow-cloudflare",
