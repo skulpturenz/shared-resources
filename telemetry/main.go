c506c28ac4ecbeb8c80fc5d463571cccbe103dbf
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2025-06-13 23:20:42 +0000

    chore: update certbot ssl request

--- telemetry/main.go
+++ telemetry/main.go
@@ -83,7 +83,7 @@ func main() {
 				sudo chmod 0600 /etc/letsencrypt/dnscloudflare.ini &&
 				sudo chmod +x /etc/letsencrypt/renewal-hooks/deploy/reload-services.sh &&
 				sudo chmod 0600 /etc/letsencrypt/renewal-hooks/deploy/reload-services.sh &&
-				sudo certbot certonly -d rollout.skulpture.xyz \
+				sudo certbot certonly -d telemetry.skulpture.xyz \
 					--dns-cloudflare --dns-cloudflare-credentials /etc/letsencrypt/dnscloudflare.ini \
 					--non-interactive --agree-tos \
 					--register-unsafely-without-email \
